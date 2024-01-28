beaa84f7c1258d7340803b5e829977386d066617
Prd46 <prd46@drexel.edu>
2024-01-27 21:12:50 -0500

    Footer designs

---
+++ components/footer.php
@@ -0,0 +1,30 @@
+<footer>
+        <div class="footer_buttons">
+            <a href="journal.php">
+                <div class="footer_button flex column aicenter">
+                    <div class="footer_icon">
+                        <img class="icon" src="media/icons/pencil.png"/>
+                    </div>
+                    <p class="footer_button_text LM">Journal</p>
+                </div>
+            </a>
+            <a href="index.php">
+                <div class="footer_button flex column aicenter">
+                    <div class="footer_homeButton_oval">
+                        <img class="icon" src="media/icons/House.png"/>
+                    </div>
+                    <p class="footer_button_text LM">Explore</p>
+                </div>
+            </a>
+            <a href="contact.php">
+                <div class="footer_button flex column aicenter">
+                    <div class="footer_icon">
+                        <img class="icon" src="media/icons/SOS.png"/>
+                    </div>
+                    <p class="footer_button_text LM">S.O.S</p>
+                </div>
+            </a>   
+        </div>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,3 +3,31 @@
   $page_name = 'Home'; // Gives a value if page name is missing
   include_once __DIR__ . '/components/header.php'
 ?>
+
+<main>
+        <div class="index_main_widget">
+            <div class="index_main_widget_header">
+                <img class="icon" src="~"/>
+                <h3 class="index_main_widget_heading TS">Daily Affirmation</h3>
+            </div>
+            <div class="index_main_widget_content BS">I am loved and worthy</div>
+        </div>
+        <div class="index_main_pageButtons">
+            <a href="reflectHome.html">
+                <div class="index_main_pageButton">Reflect</div>
+            </a>
+            <a href="relaxHome.html">
+                <div class="index_main_pageButton">Relax</div>
+            </a>
+            <a href="inspireHome.html">
+                <div class="index_main_pageButton">Inspire</div>
+            </a>
+            <a href="playHome.html">
+                <div class="index_main_pageButton">Play</div>
+            </a>
+        </div>
+    </main>
+
+<?php 
+  include_once __DIR__ . '/components/footer.php'
+?>
