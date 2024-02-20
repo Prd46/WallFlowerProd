7c69bc7f2c778122156ae7ca4a4d58e89379fcbd
Tony Chen <91432463+tc3293@users.noreply.github.com>
2024-02-20 10:01:04 -0500

    update 2/20/2024

--- sos.php
+++ sos.php
@@ -51,7 +51,7 @@
                         </div>
                         <div class="leaf_card_caption">
                             <p class="BM"></p>
-                            <img class="icon-4" src="media/icons/edit.svg">
+                            <img class="icon-4" src="media/icons/bookmark.svg">
                         </div>
                     </div>
                 </div>
@@ -73,7 +73,7 @@
                         </div>
                         <div class="leaf_card_caption">
                             <p class="BM"></p>
-                            <img class="icon-4" src="media/icons/edit.svg">
+                            <img class="icon-4" src="media/icons/bookmark.svg">
                         </div>
                     </div>
                 </div>
@@ -162,5 +162,6 @@ font-weight: 600;
   color: #FF0000;
   font-size: 18px;
 }
+
 
 </style>
