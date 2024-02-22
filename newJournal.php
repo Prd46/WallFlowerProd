17bd247ef79224398f71ead808a1a5f401344f38
Prd46 <prd46@drexel.edu>
2024-02-22 15:33:33 -0500

    Fixing all the code

--- newJournal.php
+++ newJournal.php
@@ -9,26 +9,25 @@ $query = 'SELECT * FROM JournalEntries';
 $result = mysqli_query($db_connection, $query);
 
 ?>
-<div class="main_label">
-            <img class="icon main_label_icon" src="media/icons/blackbookmark.svg"/>
-            <h1 class="main_label_header TL">New Journal</h1>
-        </div>
+
 <main>
-    <form action="includes/process_posts.php" method="POST">
+<div class="main_label">
+            <div class="main_label_header">
+                <img class="icon main_label_icon" src="media/icons/edit.svg"/>
+                <h1 class="main_label_header TL">New Journal</h1>
+            </div>
+            <p class="BM main_label_caption">
+            Write how you feel.
+            </p>
+        </div>
+<a href="journal.php"><- Return</a>
+    <form class="journal_box" action="includes/process_posts.php" method="POST">
 
     <input class="journal_emoji" type="text" id="emojiPath" name="emojiPath"><label for="emojiPath">Emoji</label>
     <input class="journal_title" type="text" id="title" name="title"><label for="title">Title</label> 
-    <!-- <input class="journal_text" type="text" id="entryText" name="entryText"><label for="title">Text</label>  -->
-    <div class="journal_text_area">
-        <label for="entryText">Enter Text Here</label>
-                <br>
-                <textarea class="journalText js-tinymce" name="entryText" id="entryText" cols="30"
-                  rows="10"></textarea>
-    </div>
+    <textarea class="journal_text" type="text" id="entryText" name="entryText"></textarea><label for="title">Text</label> 
     <input type="submit" value="submit" class="editSubmit">
     </form>
 </main>
 <script src="scripts/app.js"></script>
-<?php 
-  include_once __DIR__ . '/components/footer.php'
-?>
+<?php include "components/footer.php" ?>
